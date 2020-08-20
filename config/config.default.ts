ab9e96949277b3f2b7572861f01f61d88372aaba
hackycy <qa894178522@qq.com>
2020-08-20 16:01:09 +0800

    chore：add static config

--- config/config.default.ts
+++ config/config.default.ts
@@ -1,4 +1,5 @@
 import { EggAppConfig, EggAppInfo, PowerPartial } from 'egg';
+import * as path from 'path';
 
 export default (appInfo: EggAppInfo) => {
   const config = {} as PowerPartial<EggAppConfig>;
@@ -13,6 +14,17 @@ export default (appInfo: EggAppInfo) => {
   // add your special config in here
   const bizConfig = {
     sourceUrl: `https://github.com/eggjs/examples/tree/master/${appInfo.name}`,
+  };
+
+  // static config
+  config.static = {
+    prefix: '/',
+    dir: path.join(appInfo.baseDir, 'app/public'),
+    dynamic: true,
+    preload: false,
+    // maxAge: 31536000,
+    maxAge: 0,
+    buffer: false,
   };
 
   // the return config will combines to EggAppConfig
