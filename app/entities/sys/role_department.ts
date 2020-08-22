f4d36fa4adc9e1a552627376824a7a287513b43f
hackycy <qa894178522@qq.com>
2020-08-22 15:24:14 +0800

    feature：增加系统角色部门关系实体

---
+++ app/entities/sys/role_department.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import BaseEntity from '../base';
+
+@Entity({ name: 'sys_role_department' })
+export default class SysRoleDepartment extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'bigint', name: 'role_id' })
+  roleId: number;
+
+  @Column({ type: 'bigint', name: 'department_id' })
+  departmentId: number;
+}
