6dbd75e526ad909e41931da15972ab3490c58827
hackycy <qa894178522@qq.com>
2020-08-22 15:32:12 +0800

    feature：新增用户角色关系实体类

---
+++ app/entities/sys/user_role.ts
@@ -0,0 +1,14 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import BaseEntity from '../base';
+
+@Entity({ name: 'sys_user_role' })
+export default class SysUserRole extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'bigint', name: 'user_id' })
+  userId: number;
+
+  @Column({ type: 'bigint', name: 'role_id' })
+  roleId: number;
+}
