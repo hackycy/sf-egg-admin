108c00a7aa813c82498974575ad2fd4f8275d30c
hackycy <qa894178522@qq.com>
2020-08-22 15:11:33 +0800

    feature：增加User实体类

---
+++ app/entities/sys/user.ts
@@ -0,0 +1,39 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import BaseEntity from '../base';
+
+@Entity({ name: 'sys_user' })
+export default class SysUser extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'bigint', name: 'department_id' })
+  departmentId: number;
+
+  @Column()
+  name: string;
+
+  @Column({ unique: true })
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column({ name: 'nick_name', nullable: true })
+  nickName: string;
+
+  @Column({ name: 'head_img', nullable: true })
+  headImd: string;
+
+  @Column({ nullable: true })
+  email: string;
+
+  @Column({ nullable: true })
+  phone: string;
+
+  @Column({ nullable: true })
+  remark: string;
+
+  @Column({ type: 'tinyint', length: 4, nullable: true, default: 1 })
+  status: number;
+
+}
