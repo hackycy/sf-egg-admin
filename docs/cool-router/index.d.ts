8e658613c66580cdfbec50fc102de860f537c0e2
hackycy <qa894178522@qq.com>
2020-08-25 11:26:03 +0800

    docs：修复复制错误文件

--- docs/cool-router/index.d.ts
+++ docs/cool-router/index.d.ts
@@ -1,80 +1,79 @@
-import { Controller, Context } from 'egg';
-import { Brackets } from 'typeorm';
-interface ResOp {
-    data?: any;
-    isFail?: boolean;
-    code?: number;
-    message?: string;
+import { Application } from 'egg';
+import { Middleware } from 'koa';
+
+/** http装饰器方法类型 */
+declare type HttpFunction = (url: string, ...beforeMiddlewares: Middleware[]) => any;
+
+declare class RouterDecorator {
+    get: HttpFunction;
+    post: HttpFunction;
+    patch: HttpFunction;
+    del: HttpFunction;
+    options: HttpFunction;
+    put: HttpFunction;
+    all: HttpFunction;
+    /**
+     * 记录各个class的prefix以及相关中间件
+     * 最后统一设置
+     * @private
+     * @static
+     * @type {ClassPrefix}
+     * @memberof RouterDecorator
+     */
+    private static __classPrefix__;
+    /**
+     * 记录各个routerUrl的路由配置
+     * 最后统一设置
+     * @private
+     * @static
+     * @type {Router}
+     * @memberof RouterDecorator
+     */
+    private static __router__;
+
+    constructor ();
+
+    /** 推入路由配置 */
+    private __setRouter__;
+
+    /**
+     * 装饰Controller class的工厂函数
+     * 为一整个controller添加prefix
+     * 可以追加中间件
+     * @param {string} prefixUrl
+     * @param {...Middleware[]} beforeMiddlewares
+     * @param {[]} baseFn 配置通用的接口 可选 page、add、update、delete、info、list
+     * @returns 装饰器函数
+     * @memberof RouterDecorator
+     */
+    prefix (prefixUrl: string, baseFn?: any[], ...beforeMiddlewares: Middleware[]): (targetControllerClass: any) => any;
+
+     /**
+     * 装饰Controller class的工厂函数
+     * 为一整个controller添加prefix
+     * 可以追加中间件
+     * @param {string} prefixUrl
+     * @param {...Middleware[]} beforeMiddlewares
+     * @param {[]} baseFn 配置通用的接口 可选 page、add、update、delete、info、list
+     * @returns 装饰器函数
+     * @memberof RouterDecorator
+     */
+    prefix (baseFn?: any[], ...beforeMiddlewares: Middleware[]): (targetControllerClass: any) => any;
+
+    /**
+     * 注册路由
+     * 路由信息是通过装饰器收集的
+     * @export
+     * @param {Application} app eggApp实例
+     * @param {string} [options={ prefix: '' }] 举例： { prefix: '/api' }
+     */
+    static initRouter (app: Application, options?: {
+        prefix: string;
+    }): void;
 }
-interface PageOp {
-    keyWordLikeFields?: string[];
-    where?: Brackets;
-    fieldEq?: string[];
-    addOrderBy?: {};
-}
-/**
- * 控制器基类
- */
-export declare abstract class BaseController extends Controller {
-    protected entity: any;
-    protected OpService: any;
-    protected pageOption: PageOp | undefined;
-    protected constructor(ctx: Context);
-    /**
-     * 初始化
-     */
-    protected init(): void;
-    /**
-     * 设置服务
-     * @param service
-     */
-    protected setService(service: any): void;
-    /**
-     * 配置分页查询
-     * @param option
-     */
-    protected setPageOption(option: PageOp): void;
-    /**
-     * 设置操作实体
-     * @param entity
-     */
-    protected setEntity(entity: any): void;
-    /**
-     * 获得query请求参数
-     */
-    protected getQuery(): import("egg").PlainObject<string>;
-    /**
-     * 获得body请求参数
-     */
-    protected getBody(): any;
-    /**
-     * 分页查询数据
-     */
-    protected page(): Promise<void>;
-    /**
-     * 数据列表
-     */
-    protected list(): Promise<void>;
-    /**
-     * 信息
-     */
-    protected info(): Promise<void>;
-    /**
-     * 新增
-     */
-    protected add(): Promise<void>;
-    /**
-     * 修改
-     */
-    protected update(): Promise<void>;
-    /**
-     * 删除
-     */
-    protected delete(): Promise<void>;
-    /**
-     * 返回数据
-     * @param op 返回配置，返回失败需要单独配置
-     */
-    protected res(op?: ResOp): void;
-}
-export {};
+
+/** 暴露注册路由方法 */
+export declare const initRouter: typeof RouterDecorator.initRouter;
+declare const _default: RouterDecorator;
+/** 暴露实例的prefix和http的各个方法 */
+export default _default;
