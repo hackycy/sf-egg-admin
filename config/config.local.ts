5b5cc2dc0466ce93573f604b9ec0136f191e72e4
hackycy <qa894178522@qq.com>
2020-08-30 22:53:29 +0800

    fix：bigint return is string
    https://www.shangliushuai.cn/2020/01/17/Node后台开发中使用TypeOrm遇到的问题/

--- config/config.local.ts
+++ config/config.local.ts
@@ -18,6 +18,17 @@ export default () => {
       synchronize: false,
       logging: true,
       timezone: '+0800',
+      /**
+       * JavaScript对数据库中int和bigint的区别对待：
+       * 刚开始开发中，线下测试数据库id字段采用int，数据库SELECT操作返回的结果是Number，但是使用bigint，数据库返回的为String，
+       * 初步猜想是因为bigint的值范围会超过Number，所以采用String。但是这样会对我们业务产生巨大影戏那个，一方面，DTO校验会无法通过，另一方面，问题1中的业务逻辑会受影响。
+       * 经过查找各方文档，解决方案是在数据库连接配置中配置：
+       * "supportBigNumbers": false
+       * 可以配置这个的原因是我们的业务ID距离Number的上线远远达不到，所以可以用这种方式让
+       * bigint也返回Number。
+       * 但是这样配置，TypeOrm插入操作的返回值中的identifiers字段中的id还是String，所以问题1中的处理方式也要对String进行parseInt操作。
+       */
+      supportBigNumbers: false,
     },
   };
 
