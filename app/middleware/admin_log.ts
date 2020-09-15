3682c441be203dd2e234d53da126c462d87ffb75
hackycy <qa894178522@qq.com>
2020-09-15 16:55:15 +0800

    fix：日志只记录/admin开头的url

--- app/middleware/admin_log.ts
+++ app/middleware/admin_log.ts
@@ -7,7 +7,7 @@ export default function adminLog(): any {
   return async (ctx: Context, next: () => Promise<any>) => {
     const { url } = ctx;
     // 该接口不做记录/admin/sys/log/page
-    if (!url.startsWith('/admin/sys/log/page')) {
+    if (url.startsWith('/admin') && !url.startsWith('/admin/sys/log/page')) {
       ctx.service.admin.sys.log.save(url.split('?')[0], ctx.req.method === 'GET' ? ctx.request.query : ctx.request.body, ctx.token ? ctx.token.uid : null);
     }
     await next();
