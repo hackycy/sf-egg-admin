2226446e0b1c4752188b0ef8278decf3c012ca82
hackycy <qa894178522@qq.com>
2020-09-22 12:20:22 +0800

    fix：增加图片空间info与type模型类

---
+++ app/entities/admin/image/space/info.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity } from '../../../base';
+
+@Entity({ name: 'image_space_info' })
+export default class ImageSpaceInfo extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ name: 'type_id', type: 'bigint', nullable: true })
+  typeId: number;
+
+  @Column({ type: 'varchar', length: 500 })
+  url: string;
+
+  @Column()
+  extra: string;
+
+}
---
+++ app/entities/admin/image/space/type.ts
@@ -0,0 +1,12 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity } from '../../../base';
+
+@Entity({ name: 'image_space_type' })
+export default class ImageSpaceType extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  name: string;
+
+}
