f7b50153769826e27e65c7a11ba1e13f453da135
hackycy <qa894178522@qq.com>
2020-10-06 22:52:21 +0800

    feat：完善部门Dto

--- app/dto/admin/sys/dept.ts
+++ app/dto/admin/sys/dept.ts
@@ -2,25 +2,59 @@ import {
   IsInt,
   IsNumberString,
   ArrayNotEmpty,
+  IsString,
+  Min,
+  ValidateIf,
 } from 'class-validator';
 import { Expose } from 'class-transformer';
 
-export class DeleteDeptDto {
+export class CreateDeptDto {
 
+  @IsString()
+  @Expose()
+  departmentName: string;
+
+  @IsInt()
+  @Expose()
+  parentDepartmentId: number;
+
+  @ValidateIf((_o, v) => { return !(v === undefined || v === null); })
+  @IsInt()
+  @Min(0)
+  @Expose()
+  orderNum: number;
+}
+
+export class UpdateDeptDto {
+  @IsInt()
+  @Expose()
+  id: number;
+
+  @IsString()
+  @Expose()
+  name: string;
+
+  @IsInt()
+  @Expose()
+  parentId: number;
+
+  @ValidateIf((_o, v) => { return !(v === undefined || v === null); })
+  @IsInt()
+  @Min(0)
+  @Expose()
+  orderNum: number;
+}
+
+export class DeleteDeptDto {
   @IsInt()
   @Expose()
   departmentId: number;
 }
 
 export class InfoDeptDto {
-
   @IsNumberString()
   @Expose()
   departmentId: string;
-}
-
-export class UpdateDeptDto {
-
 }
 
 export class TransferDeptDto {
