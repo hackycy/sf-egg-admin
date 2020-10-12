942b29efd9d732dc0aee919ba645af49bff0199e
hackycy <qa894178522@qq.com>
2020-10-12 17:07:47 +0800

    fix：await next()问题导致请求状态记录有误

--- app/middleware/admin_req_log.ts
+++ app/middleware/admin_req_log.ts
@@ -6,12 +6,11 @@ import { Context } from 'egg';
 export default function adminReqLog(): any {
   return async (ctx: Context, next: () => Promise<any>) => {
     const { url } = ctx;
+    await next();
     // 该接口不做记录/admin/sys/log/page
     if (url.startsWith('/admin') && !url.startsWith('/admin/sys/log/page')) {
-      // TODO status 不一定准确，当抛出异常时执行顺序这里就是错误的了
       ctx.service.admin.sys.reqLog.save(url.split('?')[0],
         ctx.req.method === 'GET' ? ctx.request.query : ctx.request.body, ctx.status, ctx.req.method, ctx.token ? ctx.token.uid : null);
     }
-    await next();
   };
 }
