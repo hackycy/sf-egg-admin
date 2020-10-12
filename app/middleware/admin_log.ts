f096166346dd2ce249af49bfe452420ebc5577dc
hackycy <qa894178522@qq.com>
2020-10-12 15:47:47 +0800

    fix：修复参数类型错误

--- app/middleware/admin_log.ts
+++ app/middleware/admin_log.ts
@@ -8,7 +8,8 @@ export default function adminLog(): any {
     const { url } = ctx;
     // 该接口不做记录/admin/sys/log/page
     if (url.startsWith('/admin') && !url.startsWith('/admin/sys/log/page')) {
-      ctx.service.admin.sys.log.save(url.split('?')[0], ctx.req.method === 'GET' ? ctx.request.query : ctx.request.body, ctx.token ? ctx.token.uid : null);
+      ctx.service.admin.sys.log.save(url.split('?')[0],
+        ctx.req.method === 'GET' ? ctx.request.query : ctx.request.body, ctx.token ? ctx.token.uid : null);
     }
     await next();
   };
--- app/service/admin/sys/log.ts
+++ app/service/admin/sys/log.ts
@@ -8,12 +8,12 @@ export default class SysLogService extends BaseService {
   /**
    * 记录日志
    */
-  async save(url: string, params: string, userId: number) {
+  async save(url: string, params: string, userId: number | null) {
     const ip = this.getHelper().getReqIP();
     await this.getRepo().admin.sys.Log.insert({
       action: url,
       params: JSON.stringify(params),
-      userId,
+      userId: userId === null ? undefined : userId,
       ip,
     });
   }
