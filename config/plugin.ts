ac586c356da2a45813a1bf574a760ad0d353b968
hackycy <qa894178522@qq.com>
2020-10-13 10:49:15 +0800

    chore：remove some view

--- config/plugin.ts
+++ config/plugin.ts
@@ -22,10 +22,6 @@ const plugin: EggPlugin = {
     enable: true,
     package: 'egg-global-header',
   },
-  nunjucks: {
-    enable: true,
-    package: 'egg-view-nunjucks',
-  },
 };
 
 export default plugin;
