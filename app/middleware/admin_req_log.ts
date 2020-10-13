a8802071fd4fe87f89be61febae6b595c07f3611
hackycy <qa894178522@qq.com>
2020-10-13 17:44:29 +0800

    fix：fix路由地址变更导致请求追踪记录有误

--- app/middleware/admin_req_log.ts
+++ app/middleware/admin_req_log.ts
@@ -8,7 +8,7 @@ export default function AdminReqLog(): any {
     await next();
     const { url } = ctx;
     // 该接口不做记录/admin/sys/log/page
-    if (url.startsWith('/admin') && !url.startsWith('/admin/sys/log/page')) {
+    if (url.startsWith('/admin') && !url.startsWith('/admin/sys/req-log/page')) {
       ctx.service.admin.sys.reqLog.save(url.split('?')[0],
         ctx.req.method === 'GET' ? ctx.request.query : ctx.request.body, ctx.status, ctx.req.method, ctx.token ? ctx.token.uid : null);
     }
