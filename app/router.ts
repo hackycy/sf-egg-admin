009e188283dc6c53859817b462325d74a5baf833
hackycy <qa894178522@qq.com>
2020-10-13 10:54:50 +0800

    chore：remove constant route

--- app/router.ts
+++ app/router.ts
@@ -2,11 +2,8 @@ import { Application } from 'egg';
 import { initRouter } from './decorator/router_register';
 
 export default (app: Application) => {
-  const { controller, router } = app;
+  // const { controller, router } = app;
   // router.all('/a', controller.welcome.index);
   // 使用@Route进行注册路由
   initRouter(app);
-  // share server router
-  router.get('/', controller.app.home.index);
-  router.get('/login', controller.app.home.login);
 };
