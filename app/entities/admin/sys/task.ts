605953747f358f9487aa15d598fcd8cb97c8c8ad
hackycy <qa894178522@qq.com>
2020-10-14 15:30:34 +0800

    feat：增加任务及任务日志实体类

---
+++ app/entities/admin/sys/task.ts
@@ -0,0 +1,39 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity } from '../../base';
+
+@Entity({ name: 'sys_task' })
+export default class SysDepartment extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ type: 'varchar', length: 50, unique: true })
+  name: string;
+
+  @Column()
+  service: string;
+
+  @Column({ type: 'tinyint', default: 0 })
+  type: number;
+
+  @Column({ name: 'start_time', type: 'datetime', nullable: true })
+  startTime: Date;
+
+  @Column({ name: 'end_time', type: 'datetime', nullable: true })
+  endTime: Date;
+
+  @Column({ type: 'int', nullable: true, default: 0 })
+  limit: number;
+
+  @Column({ nullable: true })
+  cron: string;
+
+  @Column({ type: 'int', nullable: true })
+  every: number;
+
+  @Column({ type: 'varchar', length: 500, nullable: true })
+  data: string;
+
+  @Column({ nullable: true })
+  remark: string;
+
+}
---
+++ app/entities/admin/sys/task_log.ts
@@ -0,0 +1,18 @@
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { BaseEntity } from '../../base';
+
+@Entity({ name: 'sys_task_log' })
+export default class SysDepartment extends BaseEntity {
+  @PrimaryGeneratedColumn({ type: 'bigint' })
+  id: number;
+
+  @Column({ name: 'task_id', type: 'bigint' })
+  taskId: string;
+
+  @Column({ type: 'tinyint', default: 0 })
+  status: number;
+
+  @Column({ type: 'text', nullable: true })
+  detail: string;
+
+}
