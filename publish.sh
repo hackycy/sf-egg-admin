016d5cb2f74c89d21a78072870a150a305aaf9fc
hackycy <qa894178522@qq.com>
2020-11-03 22:17:51 +0800

    chore：更新部署脚本

--- publish.sh
+++ publish.sh
@@ -1,23 +1,27 @@
 # stop pre 
 cd /www/server/sf-admin/sf-egg-admin
 npm run stop
-rm -rf /www/server/sf-admin/sf-egg-admin
-rm -rf /www/server/sf-admin/sf-vue-admin
-unzip -o /www/server/sf-admin/sf-egg-admin-master.zip -d /www/server/sf-admin
-mv /www/server/sf-admin/sf-egg-admin-master /www/server/sf-admin/sf-egg-admin
-unzip -o /www/server/sf-admin/sf-vue-admin-master.zip -d /www/server/sf-admin
-mv /www/server/sf-admin/sf-vue-admin-master /www/server/sf-admin/sf-vue-admin
-cp /www/server/sf-admin/config.prod.ts /www/server/sf-admin/sf-egg-admin/config
-cp /www/server/sf-admin/.env.production /www/server/sf-admin/sf-vue-admin
+cd /www/server/sf-admin
+rm -rf sf-egg-admin
+rm -rf sf-vue-admin
+unzip sf-egg-admin-master.zip
+mv sf-egg-admin-master sf-egg-admin
+unzip sf-vue-admin-master.zip
+mv sf-vue-admin-master sf-vue-admin
+cp config.prod.ts /www/server/sf-admin/sf-egg-admin/config
+cp .env.production /www/server/sf-admin/sf-vue-admin
 cd /www/server/sf-admin/sf-egg-admin
+pwd
 npm i --registry=https://registry.npm.taobao.org
 npm run build
 npm run start
 cd /www/server/sf-admin/sf-vue-admin
+pwd
 npm i --registry=https://registry.npm.taobao.org
 npm run build:prod
 tar czvf dist.tar.gz dist
 cp dist.tar.gz /www/wwwroot/opensource.admin.si-yee.com
 cd /www/wwwroot/opensource.admin.si-yee.com
+pwd
 rm -rf dist
 tar xzvf dist.tar.gz
